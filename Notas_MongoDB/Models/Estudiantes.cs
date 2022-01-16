f15f35acbbccc98987b1c09fc05c1bb208794de7
Giancarlo Queirolo <75689047+Giank15@users.noreply.github.com>
2022-01-16 11:22:27 -0500

    Programa Completo

---
+++ Notas_MongoDB/Models/Estudiantes.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Notas_MongoDB.Models
+{
+    public class Estudiantes
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement("nombre")]
+        public string Nombre { get; set; }
+        
+        [BsonElement("nota1")]
+        public int Nota1 { get; set; }
+
+        [BsonElement("nota2")]
+        public int Nota2 { get; set; }
+    }
+}
